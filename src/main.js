fafab7fb608c6a454807f597990c3b4aede87387
Joey J <github@wyxos.com>
2023-06-06 23:01:37 +0400

    feat: release v1.3.0

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-const components = import.meta.glob('./components/*.vue', {eager: true})
+const components = import.meta.glob('./components/*.vue', { eager: true })
 
 const MappedComponents = {}
 
 const install = (app) => {
-    Object.keys(components).forEach((key) => {
-        const name = components[key].default.name
-        const component = components[key].default
-        app.component(name, component)
-        app.component(name.replace('Wyxos', 'W'), component)
-        MappedComponents[name] = component
-    })
+  Object.keys(components).forEach((key) => {
+    const name = components[key].default.name
+    const component = components[key].default
+    app.component(name, component)
+    app.component(name.replace('Wyxos', 'W'), component)
+    MappedComponents[name] = component
+  })
 }
 
 export default {
-    install,
-    ...MappedComponents
+  install,
+  ...MappedComponents
 }
