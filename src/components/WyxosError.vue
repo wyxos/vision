d69cb97e091a66e29f4cc090d92265fe7a138505
Joey J <joey.jaulimsing@live.com>
2023-06-26 03:54:12 +0400

    chore: error and progress components

---
+++ src/components/WyxosError.vue
@@ -0,0 +1,29 @@
+<script>
+import {defineComponent} from 'vue'
+import FormBuilder from "../utilities/FormBuilder";
+import useFormErrors from "../utilities/useFormErrors.js";
+
+export default defineComponent({
+    name: "WyxosError",
+    props: {
+        form: {
+            type: FormBuilder,
+            default: null
+        },
+        name: {
+            type: String,
+            required: true
+        }
+    },
+    setup(){
+        return {
+            errors: useFormErrors()
+        }
+    }
+})
+</script>
+
+<template>
+    <p v-if="form?.getError(name).message">{{ form.getError(name).message }}</p>
+    <p v-else-if="errors.get(name)?.message">{{ errors.get(name).message }}</p>
+</template>
