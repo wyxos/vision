9874e691f5ec634f4db1abe81dc72096915f6bc8
Joey J <github@wyxos.com>
2023-06-26 04:16:58 +0400

    fix: linting config

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,38 +1,45 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslint from 'vite-plugin-eslint'
 import path from 'path'
 import utilityImportPlugin from '../utility-import-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: __dirname,
-  server: {
-    port: 3000
-  },
-  plugins: [utilityImportPlugin(), vue()],
-  resolve: {
-    dedupe: ['vue']
-  },
-  build: {
-    outDir: '../dist',
-    lib: {
-      entry: path.resolve(__dirname, '../src/main.js'),
-      name: 'Vision',
-      // the proper extensions will be added
-      fileName: 'vision'
+    root: __dirname,
+    server: {
+        port: 3000
     },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        exports: 'named',
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue'
+    plugins: [
+        utilityImportPlugin(),
+        vue(),
+        eslint({
+            fix: false
+        })
+    ],
+    resolve: {
+        dedupe: ['vue']
+    },
+    build: {
+        outDir: '../dist',
+        lib: {
+            entry: path.resolve(__dirname, '../src/main.js'),
+            name: 'Vision',
+            // the proper extensions will be added
+            fileName: 'vision'
+        },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: ['vue'],
+            output: {
+                exports: 'named',
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    vue: 'Vue'
+                }
+            }
         }
-      }
     }
-  }
 })
