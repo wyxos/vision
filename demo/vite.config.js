3ebaaf44a60492e2d7cb945761ec2253a38d36fd
Joey J <github@wyxos.com>
2023-06-26 04:17:42 +0400

    feat: release v1.3.3

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,4 +1,4 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
 import path from 'path'
@@ -6,40 +6,40 @@ import utilityImportPlugin from '../utility-import-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    root: __dirname,
-    server: {
-        port: 3000
+  root: __dirname,
+  server: {
+    port: 3000
+  },
+  plugins: [
+    utilityImportPlugin(),
+    vue(),
+    eslint({
+      fix: false
+    })
+  ],
+  resolve: {
+    dedupe: ['vue']
+  },
+  build: {
+    outDir: '../dist',
+    lib: {
+      entry: path.resolve(__dirname, '../src/main.js'),
+      name: 'Vision',
+      // the proper extensions will be added
+      fileName: 'vision'
     },
-    plugins: [
-        utilityImportPlugin(),
-        vue(),
-        eslint({
-            fix: false
-        })
-    ],
-    resolve: {
-        dedupe: ['vue']
-    },
-    build: {
-        outDir: '../dist',
-        lib: {
-            entry: path.resolve(__dirname, '../src/main.js'),
-            name: 'Vision',
-            // the proper extensions will be added
-            fileName: 'vision'
-        },
-        rollupOptions: {
-            // make sure to externalize deps that shouldn't be bundled
-            // into your library
-            external: ['vue'],
-            output: {
-                exports: 'named',
-                // Provide global variables to use in the UMD build
-                // for externalized deps
-                globals: {
-                    vue: 'Vue'
-                }
-            }
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        exports: 'named',
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
         }
+      }
     }
+  }
 })
--- src/components/WyxosProgress.vue
+++ src/components/WyxosProgress.vue
@@ -1,28 +1,29 @@
 <script>
-import {defineComponent} from 'vue'
+import { defineComponent } from 'vue'
 export default defineComponent({
-    name: "WyxosProgress",
-    props: {
-        showValue: {
-            type: Boolean,
-            default: true
-        },
-        value: {
-            type: Number,
-            required: true
-        },
-        max: {
-            type: Number,
-            required: true
-        },
-        format: {
-            type: String,
-            default: 'percent'
-        },
+  name: 'WyxosProgress',
+  props: {
+    showValue: {
+      type: Boolean,
+      default: true
+    },
+    value: {
+      type: Number,
+      required: true
+    },
+    max: {
+      type: Number,
+      required: true
+    },
+    format: {
+      type: String,
+      default: 'percent'
     }
+  }
 })
 </script>
 
 <template>
-    <progress :value="value" :max="max"></progress><span v-if="showValue">{{ value }} / {{ max }}</span>
+  <progress :value="value" :max="max"></progress
+  ><span v-if="showValue">{{ value }} / {{ max }}</span>
 </template>
