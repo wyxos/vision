8ce22e577b65925b6d90c4fa65feab7c2a6eab10
Joey J <joey.jaulimsing@live.com>
2023-10-10 17:31:28 +0400

    chore: linted

--- release.mjs
+++ release.mjs
@@ -66,7 +66,7 @@ const release = async () => {
     console.log(chalk.green(`Successfully released version ${version}`));
     console.log(chalk.green("Publishing to npm..."));
     execSyncOut("npm login");
-    execSyncOut("npm publish --access public");
+    execSyncOut("npm publish --access public --verbose");
   } catch (error) {
     console.error(chalk.red("Release process failed. Error:", error));
   }
