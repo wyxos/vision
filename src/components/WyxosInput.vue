8d22ac71cf6527325abb97200fd94dac8d573d22
Joey J <joey.jaulimsing@live.com>
2023-12-11 20:28:05 +0400

    chore: linted

--- src/components/WyxosInput.vue
+++ src/components/WyxosInput.vue
@@ -100,15 +100,15 @@ export default {
 <template>
   <o-field :label="label" :class="fieldClass" v-bind="getError">
     <o-input
-        :readonly="readonly"
-        :class="inputClass"
-        :root-class="inputRootClass"
-        :name="name"
-        :type="type"
-        :clearable="clearable"
-        :disabled="disabled"
-        :model-value="modelValue"
-        :placeholder="placeholder"
-        @update:model-value="onInput($event)"></o-input>
+      :readonly="readonly"
+      :class="inputClass"
+      :root-class="inputRootClass"
+      :name="name"
+      :type="type"
+      :clearable="clearable"
+      :disabled="disabled"
+      :model-value="modelValue"
+      :placeholder="placeholder"
+      @update:model-value="onInput($event)"></o-input>
   </o-field>
 </template>
--- src/utilities/useFormErrors.js
+++ src/utilities/useFormErrors.js
@@ -1,79 +1,79 @@
-import {reactive} from 'vue'
+import { reactive } from 'vue'
 
 const formErrors = reactive({
-    default: []
+  default: []
 })
 
 export default function useFormErrors() {
-    return {
-        createBag(bag) {
-            if (!formErrors[bag]) {
-                formErrors[bag] = []
-            }
-        },
-        set(error, bag = 'default') {
-            const hasValidationErrors =
-                error.response && error.response.data && error.response.data.errors
+  return {
+    createBag(bag) {
+      if (!formErrors[bag]) {
+        formErrors[bag] = []
+      }
+    },
+    set(error, bag = 'default') {
+      const hasValidationErrors =
+        error.response && error.response.data && error.response.data.errors
 
-            if (!hasValidationErrors) {
-                throw error
-            }
+      if (!hasValidationErrors) {
+        throw error
+      }
 
-            formErrors[bag] = Object.keys(error.response.data.errors).map((key) => {
-                return {
-                    key,
-                    message: error.response.data.errors[key][0]
-                }
-            })
-        },
-        get(key, bag = 'default') {
-            const target = formErrors[bag]
-
-            if (!target) {
-                return {
-                    message: '',
-                    variant: ''
-                }
-            }
-
-            const match = target.find((error) =>
-                Array.isArray(key) ? key.includes(error.key) : error.key === key
-            )
-
-            console.log('target', key, match)
-
-            if (!match) {
-                return {
-                    message: '',
-                    variant: ''
-                }
-            }
-
-            return {
-                message: match.message,
-                variant: 'danger'
-            }
-        },
-        clear(key = null, bag = 'default') {
-            if (key) {
-                const target = formErrors[bag]
-
-                if (!target) {
-                    console.warn(`Bag ${bag} is not defined.`)
-                    return
-                }
-
-                const index = target.findIndex((error) => error.key === key)
-
-                target.splice(index, 1)
-
-                return
-            }
-
-            formErrors[bag] = []
-        },
-        all(bag = 'default') {
-            return formErrors[bag]
+      formErrors[bag] = Object.keys(error.response.data.errors).map((key) => {
+        return {
+          key,
+          message: error.response.data.errors[key][0]
         }
+      })
+    },
+    get(key, bag = 'default') {
+      const target = formErrors[bag]
+
+      if (!target) {
+        return {
+          message: '',
+          variant: ''
+        }
+      }
+
+      const match = target.find((error) =>
+        Array.isArray(key) ? key.includes(error.key) : error.key === key
+      )
+
+      console.log('target', key, match)
+
+      if (!match) {
+        return {
+          message: '',
+          variant: ''
+        }
+      }
+
+      return {
+        message: match.message,
+        variant: 'danger'
+      }
+    },
+    clear(key = null, bag = 'default') {
+      if (key) {
+        const target = formErrors[bag]
+
+        if (!target) {
+          console.warn(`Bag ${bag} is not defined.`)
+          return
+        }
+
+        const index = target.findIndex((error) => error.key === key)
+
+        target.splice(index, 1)
+
+        return
+      }
+
+      formErrors[bag] = []
+    },
+    all(bag = 'default') {
+      return formErrors[bag]
     }
+  }
 }
