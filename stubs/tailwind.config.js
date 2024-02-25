30daffb2921a8a0f642b6b5c74c4a2c730488773
Joey J <github@wyxos.com>
2024-02-26 00:06:18 +0400

    chore: linted

--- stubs/tailwind.config.js
+++ stubs/tailwind.config.js
@@ -2,20 +2,20 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-        './resources/**/*.css'
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans]
-            }
-        }
-    },
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+    './resources/**/*.css'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
 }
