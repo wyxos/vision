067e63f1366eb86b810147b2098bed968977b4d3
Joey J <github@wyxos.com>
2024-12-22 16:05:22 +0400

    chore: alignments

--- src/utilities/Action.js
+++ src/utilities/Action.js
@@ -1,109 +1,144 @@
-import { ref } from 'vue'
+import {ref} from 'vue'
 import axios from 'axios'
 
 export default class Action {
-  url = ''
-  processing = ref(null)
-  beforeCallback = null
-  afterCallback = null
-  error = null
+    url = ''
+    processing = ref(null)
+    beforeCallback = null
+    afterCallback = null
+    error = null
 
-  constructor(url) {
-    this.url = url
-  }
-
-  static create(url) {
-    return new this(url)
-  }
-
-  async patch(payload) {
-    const key = this.keyResolver ? this.keyResolver(payload) : null
-
-    this.processing.value = key || true
-
-    // Run before action callback and check if we should proceed
-    if (this.beforeCallback && this.beforeCallback(payload) === false) {
-      this.processing.value = null
-
-      return
+    constructor(url) {
+        this.url = url
     }
 
-    // await new Promise((resolve) => setTimeout(resolve, 1000))
+    static create(url) {
+        return new this(url)
+    }
 
-    try {
-      const url = typeof this.url === 'function' ? this.url(payload) : this.url
+    async get(params) {
+        const key = this.keyResolver ? this.keyResolver(params) : null
 
-      this.processing.value = false
+        this.processing.value = key || true
 
-      return axios.patch(url, payload).then((response) => {
-        this.processing.value = false
+        // Run before action callback and check if we should proceed
+        if (this.beforeCallback && this.beforeCallback(params) === false) {
+            this.processing.value = null
 
-        if (this.afterCallback) {
-          this.afterCallback(response)
+            return
         }
 
-        return response
-      })
-    } catch (error) {
-      this.processing.value = false
+        try {
+            const url = typeof this.url === 'function' ? this.url(params) : this.url
 
-      this.error = error.response?.data?.message || error.message
+            return axios.get(url, {
+                params
+            }).then((response) => {
+                this.processing.value = false
+
+                if (this.afterCallback) {
+                    this.afterCallback(response)
+                }
+
+                return response
+            })
+        } catch (error) {
+            this.error = error.response?.data?.message || error.message
+
+            this.processing.value = false
+
+            return error
+        }
     }
-  }
 
-  async delete(payload) {
-    const key = this.keyResolver ? this.keyResolver(payload) : null
+    async patch(payload) {
+        const key = this.keyResolver ? this.keyResolver(payload) : null
 
-    this.processing.value = key || true
+        this.processing.value = key || true
 
-    // Run before action callback and check if we should proceed
-    if (this.beforeCallback && this.beforeCallback(payload) === false) {
-      this.processing.value = false
+        // Run before action callback and check if we should proceed
+        if (this.beforeCallback && this.beforeCallback(payload) === false) {
+            this.processing.value = null
 
-      return
-    }
-
-    // await new Promise((resolve) => setTimeout(resolve, 1000))
-
-    try {
-      const url = typeof this.url === 'function' ? this.url(payload) : this.url
-
-      return axios.delete(url).then((response) => {
-        this.processing.value = false
-
-        if (this.afterCallback) {
-          this.afterCallback(response)
+            return
         }
 
-        return response
-      })
-    } catch (error) {
-      this.error = error.response?.data?.message || error.message
+        // await new Promise((resolve) => setTimeout(resolve, 1000))
 
-      this.processing.value = false
+        try {
+            const url = typeof this.url === 'function' ? this.url(payload) : this.url
 
-      return error
+            this.processing.value = false
+
+            return axios.patch(url, payload).then((response) => {
+                this.processing.value = false
+
+                if (this.afterCallback) {
+                    this.afterCallback(response)
+                }
+
+                return response
+            })
+        } catch (error) {
+            this.processing.value = false
+
+            this.error = error.response?.data?.message || error.message
+        }
     }
-  }
 
-  before(callback) {
-    this.beforeCallback = callback
+    async delete(payload) {
+        const key = this.keyResolver ? this.keyResolver(payload) : null
 
-    return this
-  }
+        this.processing.value = key || true
 
-  after(callback) {
-    this.afterCallback = callback
+        // Run before action callback and check if we should proceed
+        if (this.beforeCallback && this.beforeCallback(payload) === false) {
+            this.processing.value = false
 
-    return this
-  }
+            return
+        }
 
-  isProcessing(key) {
-    return this.processing.value === key
-  }
+        // await new Promise((resolve) => setTimeout(resolve, 1000))
 
-  setKeyResolver(resolver) {
-    this.keyResolver = resolver
-    return this
-  }
+        try {
+            const url = typeof this.url === 'function' ? this.url(payload) : this.url
+
+            return axios.delete(url).then((response) => {
+                this.processing.value = false
+
+                if (this.afterCallback) {
+                    this.afterCallback(response)
+                }
+
+                return response
+            })
+        } catch (error) {
+            this.error = error.response?.data?.message || error.message
+
+            this.processing.value = false
+
+            return error
+        }
+    }
+
+    before(callback) {
+        this.beforeCallback = callback
+
+        return this
+    }
+
+    after(callback) {
+        this.afterCallback = callback
+
+        return this
+    }
+
+    isProcessing(key) {
+        return this.processing.value === key
+    }
+
+    setKeyResolver(resolver) {
+        this.keyResolver = resolver
+        return this
+    }
 }
