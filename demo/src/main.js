5276583c67a52daba03bb929f3b5836835756785
Joey J <joey@wyxos.com>
2025-06-07 03:49:44 +0400

    refactor: simplify template syntax and improve error handling in FormBuilderDemo and WyxosError components

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,10 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import { OrugaConfig } from '@oruga-ui/oruga-next'
 import '@oruga-ui/theme-oruga/dist/oruga.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 // Configure axios for demo purposes
 axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com'
 
+// Configure Oruga to use Font Awesome
+OrugaConfig.iconPack = 'fas'
+
 // Create and mount the Vue application
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(OrugaConfig)
+app.mount('#app')
